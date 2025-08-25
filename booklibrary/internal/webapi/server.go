cf3fa269d7009439215314bd9497f6c851fcf3bd
Jörg Jooss <joerg@joergjooss.de>
2025-08-25 22:44:20 +0200

    refactor: use net.JoinHostPort()
    Signed-off-by: Jörg Jooss <joerg@joergjooss.de>

--- booklibrary/internal/webapi/server.go
+++ booklibrary/internal/webapi/server.go
@@ -1,8 +1,9 @@
 package webapi
 
 import (
-	"fmt"
+	"net"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/joergjo/go-samples/booklibrary/internal/model"
@@ -11,8 +12,9 @@ import (
 // NewServer creates a new HTTP server with the given handler and port.
 func NewServer(crud model.CrudService, port int) *http.Server {
 	mux := NewMux(crud)
+	addr := net.JoinHostPort("", strconv.Itoa(port))
 	s := http.Server{
-		Addr:         fmt.Sprintf(":%d", port),
+		Addr:         addr,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
